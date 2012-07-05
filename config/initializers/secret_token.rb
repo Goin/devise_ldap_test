f7b49084203e3e808579cb4c6fd2af2b662fdc3f
Goin Tan <gointan@gmail.com>
2012-07-05 11:55:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevizeLdapTest::Application.config.secret_token = 'a8e3261165c1b8d7728eb240f805f846cc1902192ea3c255992e8fb533bc17000a9561fa93a83fef69fcb44e5355c02fc0301c630bc59b3f8b69d92d5f286207'
